02125359ea2c6b88bfde7aaa91156fc6cad6cf0e
shaun deshler <shaundeshler@shauns-MacBook-Pro.local>
2019-03-10 11:32:07 -0700

    doodling with mouse position detection

--- src/script.js
+++ src/script.js
@@ -6,19 +6,35 @@ canvas.style.width = window.innerWidth;
 canvas.style.height = window.innerHeight;
 let ctx = canvas.getContext('2d');
 
-let currentPosition = 50;
+let currentPosition = 150;
 let restLength = 150;
 let k = 0.1;
 let force;
 let distance;
 let velocity = 0;
-let damp = 0.9
+let damp = 0.9;
+let x;
+let lastX;
+let speed;
+
+let mouseSpeed = (e) => {
+  speed =  x - lastX;
+  lastX = x;
+  setTimeout(mouseSpeed, 50)
+}
+mouseSpeed()
+
+document.addEventListener('mousemove', (e) => {
+  x = e.pageX
+})
 
 let animation = () => {
   requestAnimationFrame(animation);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillRect(currentPosition, 40, 10, 10);
-  ctx.stroke()
+  if(x > 130 && x < 170){
+    currentPosition = speed + 150
+  }
+  ctx.fillRect(currentPosition, 40, 10, 10)
   distance = currentPosition - restLength;
   force = distance * -k;
   velocity *= damp;
